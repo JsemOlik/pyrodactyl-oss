0187f7a082afde46912bf2dde1114e00763e17a5
JsemOlik <github@jsemolik.dev>
2025-11-28 21:14:34 +0100

    Fixed?

--- resources/scripts/routers/DashboardRouter.tsx
+++ resources/scripts/routers/DashboardRouter.tsx
@@ -1,4 +1,3 @@
-import { Ellipsis, Gear, House, Key, Lock, CircleQuestion, CircleDollar } from '@gravity-ui/icons';
 import { useStoreState } from 'easy-peasy';
 import { Fragment, Suspense, useEffect, useRef, useState } from 'react';
 import { NavLink, Route, Routes, useLocation } from 'react-router-dom';
@@ -6,7 +5,6 @@ import { NavLink, Route, Routes, useLocation } from 'react-router-dom';
 import routes from '@/routers/routes';
 
 import DashboardContainer from '@/components/dashboard/DashboardContainer';
-import SupportContainer from '@/components/dashboard/SupportContainer';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -20,6 +18,11 @@ import { DashboardMobileMenu } from '@/components/elements/MobileFullScreenMenu'
 import MobileTopBar from '@/components/elements/MobileTopBar';
 import Logo from '@/components/elements/PyroLogo';
 import { NotFound } from '@/components/elements/ScreenBlock';
+import HugeIconsApi from '@/components/elements/hugeicons/Api';
+import HugeIconsCashIcon from '@/components/elements/hugeicons/CashIcon';
+import HugeIconsDashboardSettings from '@/components/elements/hugeicons/DashboardSettings';
+import HugeIconsHome from '@/components/elements/hugeicons/Home';
+import HugeIconsSsh from '@/components/elements/hugeicons/Ssh';
 
 import http from '@/api/http';
 
@@ -45,34 +48,27 @@ const DashboardRouter = () => {
         });
     };
 
-    // const onSelectAdminPanel = () => {
-    //     window.open(`/admin`);
-    // };
-
-      const onSelectAdminPanel = () => {
-    // open in same tab
-    window.location.href = '/admin';
-  };
+    const onSelectAdminPanel = () => {
+        window.open(`/admin`);
+    };
 
     // Define refs for navigation buttons.
-    const NavigationHome = useRef(null);
-    const NavigationSettings = useRef(null);
+    const NavigationHome = useRef<HTMLAnchorElement | null>(null);
     const NavigationSettingsBilling = useRef<HTMLAnchorElement | null>(null);
-    const NavigationApi = useRef(null);
-    const NavigationSSH = useRef(null);
-    const NavigationSupport = useRef<HTMLAnchorElement | null>(null);
+    const NavigationSettings = useRef<HTMLAnchorElement | null>(null);
+    const NavigationApi = useRef<HTMLAnchorElement | null>(null);
+    const NavigationSSH = useRef<HTMLAnchorElement | null>(null);
 
     const calculateTop = (pathname: string) => {
         // Get currents of navigation refs.
         const ButtonHome = NavigationHome.current;
-        const ButtonSettings = NavigationSettings.current;
         const ButtonSettingsBilling = NavigationSettingsBilling.current;
+        const ButtonSettings = NavigationSettings.current;
         const ButtonApi = NavigationApi.current;
         const ButtonSSH = NavigationSSH.current;
-        const ButtonSupport = NavigationSupport.current;
 
         // Perfectly center the page highlighter with simple math.
-        // Height of navigation links (56) minus highlight height (40) equals 16. 16 devided by 2 is 8.
+        // Height of navigation links (56) minus highlight height (40) equals 16. 16 divided by 2 is 8.
         const HighlightOffset: number = 8;
 
         if (pathname.endsWith(`/`) && ButtonHome != null) return (ButtonHome as any).offsetTop + HighlightOffset;
@@ -82,8 +78,7 @@ const DashboardRouter = () => {
         if (pathname.endsWith('/ssh') && ButtonSSH != null) return (ButtonSSH as any).offsetTop + HighlightOffset;
         if (pathname.includes('/billing') && ButtonSettingsBilling != null)
             return (ButtonSettingsBilling as any).offsetTop + HighlightOffset;
-        if (pathname.endsWith('/support') && ButtonSupport != null)
-            return (ButtonSupport as any).offsetTop + HighlightOffset;
+
         return '0';
     };
 
@@ -135,13 +130,20 @@ const DashboardRouter = () => {
                     <div className='relative flex flex-row items-center justify-between h-8'>
                         <NavLink to={'/'} className='flex shrink-0 h-8 w-fit'>
                             <Logo uniqueId='desktop-sidebar' />
-                            {/* <h1 className='text-[35px] font-semibold leading-[98%] tracking-[-0.05rem] mb-8'>Panel</h1> */}
                         </NavLink>
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
                                 <button className='w-10 h-10 flex items-center justify-center rounded-md text-white hover:bg-white/10 p-2 cursor-pointer'>
-                                    {' '}
-                                    <Ellipsis fill='currentColor' width={26} height={22} />
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        width='16'
+                                        height='15'
+                                        fill='currentColor'
+                                        viewBox='0 0 16 15'
+                                        className='flex shrink-0 h-full w-full'
+                                    >
+                                        <path d='M8.9375 7.3775C8.9375 7.56341 8.88252 7.74515 8.7795 7.89974C8.67649 8.05432 8.53007 8.1748 8.35877 8.24595C8.18746 8.31709 7.99896 8.33571 7.8171 8.29944C7.63525 8.26317 7.4682 8.17364 7.33709 8.04218C7.20598 7.91072 7.11669 7.74323 7.08051 7.56088C7.04434 7.37854 7.06291 7.18954 7.13386 7.01778C7.20482 6.84601 7.32498 6.69921 7.47915 6.59592C7.63332 6.49263 7.81458 6.4375 8 6.4375C8.24864 6.4375 8.4871 6.53654 8.66291 6.71282C8.83873 6.8891 8.9375 7.1282 8.9375 7.3775ZM1.625 6.4375C1.43958 6.4375 1.25832 6.49263 1.10415 6.59592C0.949982 6.69921 0.829821 6.84601 0.758863 7.01778C0.687906 7.18954 0.669341 7.37854 0.705514 7.56088C0.741688 7.74323 0.830976 7.91072 0.962088 8.04218C1.0932 8.17364 1.26025 8.26317 1.4421 8.29944C1.62396 8.33571 1.81246 8.31709 1.98377 8.24595C2.15507 8.1748 2.30149 8.05432 2.4045 7.89974C2.50752 7.74515 2.5625 7.56341 2.5625 7.3775C2.5625 7.1282 2.46373 6.8891 2.28791 6.71282C2.1121 6.53654 1.87364 6.4375 1.625 6.4375ZM14.375 6.4375C14.1896 6.4375 14.0083 6.49263 13.8542 6.59592C13.7 6.69921 13.5798 6.84601 13.5089 7.01778C13.4379 7.18954 13.4193 7.37854 13.4555 7.56088C13.4917 7.74323 13.581 7.91072 13.7121 8.04218C13.8432 8.17364 14.0102 8.26317 14.1921 8.29944C14.374 8.33571 14.5625 8.31709 14.7338 8.24595C14.9051 8.1748 15.0515 8.05432 15.1545 7.89974C15.2575 7.74515 15.3125 7.56341 15.3125 7.3775C15.3125 7.1282 15.2137 6.8891 15.0379 6.71282C14.8621 6.53654 14.6236 6.4375 14.375 6.4375Z' />
+                                    </svg>
                                 </button>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent className='z-99999' sideOffset={8}>
@@ -161,31 +163,31 @@ const DashboardRouter = () => {
                     <div aria-hidden className='mt-8 mb-4 bg-[#ffffff33] min-h-[1px] w-6'></div>
                     <ul data-pyro-subnav-routes-wrapper='' className='pyro-subnav-routes-wrapper'>
                         <NavLink to={'/'} end className='flex flex-row items-center' ref={NavigationHome}>
-                            <House width={22} height={22} fill='currentColor' />
+                            <HugeIconsHome fill='currentColor' />
                             <p>Your Servers</p>
                         </NavLink>
-                        {/* <NavLink to={'/account/api'} end className='flex flex-row items-center' ref={NavigationApi}>
-                            <Lock width={22} height={22} fill='currentColor' />
-                            <p>API Keys</p>
-                        </NavLink>
-                        <NavLink to={'/account/ssh'} end className='flex flex-row items-center' ref={NavigationSSH}>
-                            <Key width={22} height={22} fill='currentColor' />
-                            <p>SSH Keys</p>
-                        </NavLink> */}
-                        <NavLink to={'/support'} end className="flex flex-row items-center" ref={NavigationSupport}>
-                            <CircleQuestion width={22} height={22} fill='currentColor' />
-                            <p>Support</p>
-                        </NavLink>
 
-                        <div className="pyro-subnav-spacer" />
+                        {/* Optional future items
+            <NavLink to={'/account/api'} end className='flex flex-row items-center' ref={NavigationApi}>
+              <HugeIconsApi fill='currentColor' />
+              <p>API Keys</p>
+            </NavLink>
+            <NavLink to={'/account/ssh'} end className='flex flex-row items-center' ref={NavigationSSH}>
+              <HugeIconsSsh fill='currentColor' />
+              <p>SSH Keys</p>
+            </NavLink>
+            */}
+
+                        {/* Spacer pushes the following links to the bottom */}
+                        <div className='pyro-subnav-spacer' />
 
                         {/* Bottom links */}
-                        <a href={'/billing'} className="flex flex-row items-center" ref={NavigationSettingsBilling}>
-                            <CircleDollar width={22} height={22} fill="currentColor" />
+                        <a href={'/billing'} className='flex flex-row items-center' ref={NavigationSettingsBilling}>
+                            <HugeIconsCashIcon fill='currentColor' />
                             <p>Billing</p>
                         </a>
                         <NavLink to={'/account'} end className='flex flex-row items-center' ref={NavigationSettings}>
-                            <Gear width={22} height={22} fill='currentColor' />
+                            <HugeIconsDashboardSettings fill='currentColor' />
                             <p>Settings</p>
                         </NavLink>
                     </ul>
@@ -208,8 +210,6 @@ const DashboardRouter = () => {
                                         element={<Component />}
                                     />
                                 ))}
-
-                                <Route path="Support" element={<SupportContainer />} />
 
                                 <Route path='*' element={<NotFound />} />
                             </Routes>
