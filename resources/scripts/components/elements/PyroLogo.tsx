57a6437cc13740c66f2028078c6db34053e8d466
JsemOlik <github@jsemolik.dev>
2025-11-29 14:25:06 +0100

    Fixed logo

--- resources/scripts/components/elements/PyroLogo.tsx
+++ resources/scripts/components/elements/PyroLogo.tsx
@@ -44,24 +44,26 @@ const Logo = ({ className, uniqueId }: { className?: string; uniqueId?: string }
         //         </radialGradient>
         //     </defs>
         // </svg>
-        <div className='flex items-center gap--10'>
-            <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='0'
-                height='0'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='#EE8132'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className={className || 'flex h-full w-full shrink-0 ml-[-20px]'}
-            >
-                <circle cx='12' cy='12' r='1' />
-                <path d='M20.2 20.2c2.04-2.03.02-7.36-4.5-11.9-4.54-4.52-9.87-6.54-11.9-4.5-2.04 2.03-.02 7.36 4.5 11.9 4.54 4.52 9.87 6.54 11.9 4.5Z' />
-                <path d='M15.7 15.7c4.52-4.54 6.54-9.87 4.5-11.9-2.03-2.04-7.36-.02-11.9 4.5-4.52 4.54-6.54 9.87-4.5 11.9 2.03 2.04 7.36.02 11.9-4.5Z' />
-            </svg>
-            <p className='ml-[-10px] text-2xl font-bold'>Quark</p>
+        <div className='flex w-full justify-center'>
+            <div className='flex items-center gap-3'>
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='40'
+                    height='40'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='#EE8132'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className={className || 'shrink-0'}
+                >
+                    <circle cx='12' cy='12' r='1' />
+                    <path d='M20.2 20.2c2.04-2.03.02-7.36-4.5-11.9-4.54-4.52-9.87-6.54-11.9-4.5-2.04 2.03-.02 7.36 4.5 11.9 4.54 4.52 9.87 6.54 11.9 4.5Z' />
+                    <path d='M15.7 15.7c4.52-4.54 6.54-9.87 4.5-11.9-2.03-2.04-7.36-.02-11.9 4.5-4.52 4.54-6.54 9.87-4.5 11.9 2.03 2.04 7.36.02 11.9-4.5Z' />
+                </svg>
+                <p className='text-2xl font-bold leading-none'>Quark</p>
+            </div>
         </div>
     );
 };
