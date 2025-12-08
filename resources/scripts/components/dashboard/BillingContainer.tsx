1c80e3d1fe96e4d0bfdd81f6dbc19fa285e0a11b
JsemOlik <github@jsemolik.dev>
2025-12-08 12:09:26 +0100

    Animation changes

--- resources/scripts/components/dashboard/BillingContainer.tsx
+++ resources/scripts/components/dashboard/BillingContainer.tsx
@@ -249,7 +249,14 @@ const BillingContainer = () => {
     return (
         <PageContentBlock title={'Billing'} showFlashKey={'billing'}>
             {creditsEnabled && (
-                <div className='transform-gpu skeleton-anim-2 mb-6'>
+                <div
+                    className='transform-gpu skeleton-anim-2 mb-6'
+                    style={{
+                        animationDelay: '0ms',
+                        animationTimingFunction:
+                            'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                    }}
+                >
                     <div className='bg-[#ffffff08] border border-[#ffffff12] rounded-lg p-6'>
                         <div className='flex items-center justify-between flex-wrap gap-4'>
                             <div>
