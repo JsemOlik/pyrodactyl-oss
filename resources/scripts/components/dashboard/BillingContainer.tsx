01bd50989ab047f359cad255810b10f9b835d66c
JsemOlik <github@jsemolik.dev>
2025-12-08 12:35:17 +0100

    anim changes

--- resources/scripts/components/dashboard/BillingContainer.tsx
+++ resources/scripts/components/dashboard/BillingContainer.tsx
@@ -283,7 +283,14 @@ const BillingContainer = () => {
                     </div>
                 </div>
             )}
-            <div className='transform-gpu skeleton-anim-2 mb-3 sm:mb-4'>
+            <div
+                className='transform-gpu skeleton-anim-2 mb-3 sm:mb-4'
+                style={{
+                    animationDelay: '0ms',
+                    animationTimingFunction:
+                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                }}
+            >
                 <MainPageHeader title='Active Services' />
                 <PageListContainer className='p-4 flex flex-col gap-3'>
                     {!subscriptions && !error ? (
@@ -301,7 +308,7 @@ const BillingContainer = () => {
                                     key={service.id}
                                     className='transform-gpu skeleton-anim-2'
                                     style={{
-                                        animationDelay: `${index * 50 + 50}ms`,
+                                        animationDelay: '0ms',
                                         animationTimingFunction:
                                             'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
                                     }}
@@ -348,7 +355,7 @@ const BillingContainer = () => {
                                                         key={service.id}
                                                         className='transform-gpu skeleton-anim-2'
                                                         style={{
-                                                            animationDelay: `${index * 50 + 50}ms`,
+                                                            animationDelay: '0ms',
                                                             animationTimingFunction:
                                                                 'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
                                                         }}
@@ -407,7 +414,14 @@ const BillingContainer = () => {
                 <>
                     <div aria-hidden className='mt-16 mb-16 bg-[#ffffff33] min-h-[1px] w-full'></div>
 
-                    <div className='transform-gpu skeleton-anim-2 mb-3 sm:mb-4'>
+                    <div
+                        className='transform-gpu skeleton-anim-2 mb-3 sm:mb-4'
+                        style={{
+                            animationDelay: '0ms',
+                            animationTimingFunction:
+                                'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                        }}
+                    >
                         <MainPageHeader title='Credit Transaction History' />
                         <PageListContainer className='p-4 flex flex-col gap-3'>
                             {!creditTransactions && !transactionsError ? (
@@ -527,7 +541,14 @@ const BillingContainer = () => {
 
             <div aria-hidden className='mt-16 mb-16 bg-[#ffffff33] min-h-[1px] w-full'></div>
 
-            <div className='transform-gpu skeleton-anim-2 mb-3 sm:mb-4'>
+            <div
+                className='transform-gpu skeleton-anim-2 mb-3 sm:mb-4'
+                style={{
+                    animationDelay: '0ms',
+                    animationTimingFunction:
+                        'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
+                }}
+            >
                 <MainPageHeader title='Billing & Invoices' />
                 <PageListContainer className='p-4 flex flex-col gap-3'>
                     {!invoices && !invoicesError ? (
