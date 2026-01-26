057611a6308178a8498fa99ddac1fd0bbb01d62f
Oliver Steiner <github@jsemolik.dev>
2026-01-26 22:20:39 +0100

    melkanik

---
+++ app/Http/Controllers/Api/Client/Servers/SettingsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pterodactyl\Http\Controllers\Api\Client\Servers;
+
+use Pterodactyl\Models\Server;
+use Pterodactyl\Http\Controllers\Api\Client\ClientApiController;
+
+class SettingsController extends ClientApiController
+{
+    public function __call($method, $parameters)
+    {
+        return $this->proxyToDaemonController('SettingsController', $method, $parameters);
+    }
+
+    private function proxyToDaemonController(string $controllerName, string $method, array $parameters)
+    {
+        $server = $this->getServerFromParameters($parameters);
+        $server->loadMissing('node');
+
+        $daemonType = ucfirst($server->node?->daemonType ?? 'elytra');
+        $controllerClass = "Pterodactyl\\Http\\Controllers\\Api\\Client\\Servers\\{$daemonType}\\{$controllerName}";
+
+        if (!class_exists($controllerClass)) {
+            abort(500, "Controller {$controllerClass} does not exist");
+        }
+
+        $controller = app($controllerClass);
+        return $controller->$method(...$parameters);
+    }
+
+    private function getServerFromParameters(array $parameters): Server
+    {
+        foreach ($parameters as $param) {
+            if ($param instanceof Server) {
+                return $param;
+            }
+        }
+
+        abort(500, 'Server not found in parameters');
+    }
+}
