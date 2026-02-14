0fa3fbf76c1c9f43110f24e2114880ed613eb8f5
JsemOlik <github@jsemolik.dev>
2026-02-15 00:19:46 +0100

    style(ui): slightly increase console height and use red accents for stop/kill

--- resources/scripts/components/server/console/ServerConsoleContainer.tsx
+++ resources/scripts/components/server/console/ServerConsoleContainer.tsx
@@ -170,7 +170,7 @@ const ServerConsoleContainer = () => {
                                 'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
                         }}
                     >
-                        <div className='bg-gradient-to-b from-[#ffffff08] to-[#ffffff05] border-[1px] border-[#ffffff12] rounded-xl p-3 sm:p-4 hover:border-[#ffffff20] transition-all duration-150 shadow-sm'>
+                        <div className='bg-gradient-to-b from-[#ffffff08] to-[#ffffff05] border-[1px] border-[#ffffff12] rounded-xl p-3 sm:p-4 hover:border-[#ffffff20] transition-all duration-150 shadow-sm min-h-[260px]'>
                             <Console />
                         </div>
                     </div>
