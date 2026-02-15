7e4018aec48723789487e8d851631bd0e0971a37
JsemOlik <github@jsemolik.dev>
2026-02-15 14:22:23 +0100

    feat: move Players stat into ServerDetailsBlock between IP and Uptime

--- resources/scripts/components/server/console/ServerConsoleContainer.tsx
+++ resources/scripts/components/server/console/ServerConsoleContainer.tsx
@@ -89,15 +89,7 @@ const ServerConsoleContainer = () => {
                 >
                     <MainPageHeader
                         title={name}
-                        headChildren={
-                            <div className='hidden bg-color ms:block ms:inline-flex md:inline-flex md:block absolute left-0 mb-4 mt-8 flex gap-2'>
-                                {playerData && (
-                                    <p className='p-1 text-zinc-300 border-2 bg-gradient-to-b from-[#ffffff08] to-[#ffffff05] border-[#ffffff12] rounded-lg hover:border-[#ffffff20] transition-all duration-150 shadow-sm'>
-                                        Players: {playerData.online}/{playerData.max}
-                                    </p>
-                                )}
-                            </div>
-                        }
+                        headChildren={null}
                         titleChildren={
                             <div
                                 className='transform-gpu skeleton-anim-2'
@@ -107,13 +99,7 @@ const ServerConsoleContainer = () => {
                                         'linear(0,0.01,0.04 1.6%,0.161 3.3%,0.816 9.4%,1.046,1.189 14.4%,1.231,1.254 17%,1.259,1.257 18.6%,1.236,1.194 22.3%,1.057 27%,0.999 29.4%,0.955 32.1%,0.942,0.935 34.9%,0.933,0.939 38.4%,1 47.3%,1.011,1.017 52.6%,1.016 56.4%,1 65.2%,0.996 70.2%,1.001 87.2%,1)',
                                 }}
                             >
-                                <div className='inline-flex relative max-w-50 min-w-35 block ms:hidden md:hidden justify-left left-0 mb-4 flex gap-2'>
-                                    {playerData && (
-                                        <p className='p-1 text-zinc-300 border-2 bg-gradient-to-b from-[#ffffff08] to-[#ffffff05] border-[#ffffff12] rounded-lg hover:border-[#ffffff20] transition-all duration-150 shadow-sm'>
-                                            Players: {playerData.online}/{playerData.max}
-                                        </p>
-                                    )}
-                                </div>
+                                <div className='inline-flex relative max-w-50 min-w-35 block ms:hidden md:hidden justify-left left-0 mb-4 flex gap-2'></div>
                                 <PowerButtons className='flex gap-1 items-center justify-center' />
                             </div>
                         }
