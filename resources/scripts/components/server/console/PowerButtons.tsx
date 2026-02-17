1ff4f84e66f7487f16b57792cfe1eb532b1808d0
Oliver Steiner <113779745+JsemOlik@users.noreply.github.com>
2026-02-17 09:01:16 +0100

    ADIAFDSF

--- resources/scripts/components/server/console/PowerButtons.tsx
+++ resources/scripts/components/server/console/PowerButtons.tsx
@@ -108,8 +108,8 @@ const PowerButtons = ({ className }: PowerButtonProps) => {
                         status === 'offline'
                             ? {
                                   background:
-                                      'radial-gradient(124.75% 124.75% at 50.01% -10.55%, rgb(36, 36, 36) 0%, rgb(20, 20, 20) 100%)',
-                                  opacity: 0.6,
+                                      'radial-gradient(124.75% 124.75% at 50.01% -10.55%, rgb(36, 36, 36) 0%, rgb(20, 20, 20) 100%) 100%)',
+                                  opacity: 1,
                               }
                             : {
                                   background:
@@ -117,7 +117,7 @@ const PowerButtons = ({ className }: PowerButtonProps) => {
                                   opacity: 1,
                               }
                     }
-                    className='px-8 py-3 border-[1px] border-[#ffffff12] rounded-r-full rounded-l-md text-sm font-bold shadow-md transition-all cursor-pointer'
+                    className='px-8 py-3 border-[1px] border-[#ffffff12] rounded-r-full rounded-l-md text-sm font-bold shadow-md cursor-pointer'
                     disabled={status === 'offline'}
                     onClick={onButtonClick.bind(this, killable ? 'kill' : 'stop')}
                 >
