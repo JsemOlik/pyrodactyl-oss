f319e702f1865da22cc1dafe18b87e5086e2cd41
Oliver Steiner <113779745+JsemOlik@users.noreply.github.com>
2026-02-17 09:02:29 +0100

    nevermind

--- resources/scripts/components/server/console/PowerButtons.tsx
+++ resources/scripts/components/server/console/PowerButtons.tsx
@@ -109,7 +109,7 @@ const PowerButtons = ({ className }: PowerButtonProps) => {
                             ? {
                                   background:
                                       'radial-gradient(124.75% 124.75% at 50.01% -10.55%, rgb(36, 36, 36) 0%, rgb(20, 20, 20) 100%) 100%)',
-                                  opacity: 1,
+                                  opacity: 0.6,
                               }
                             : {
                                   background:
